51aaf935d8fffe49e70f671f1cfd69f2791475d0
Nihal108-bi <nihaljaisawal1@gmail.com>
2024-11-20 18:56:48 +0530

    My first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='mrnj',
+    author_email='nihaljaisawal1@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
